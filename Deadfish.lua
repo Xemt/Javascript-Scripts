fa68675e57f11ebd725dfdf740f531c136f5b116
Xemt <92655476+Xemt@users.noreply.github.com>
2022-07-28 07:34:38 -0400

    Update Deadfish.lua
    Working on it

--- Deadfish.lua
+++ Deadfish.lua
@@ -1,1 +1,7 @@
+-- Made by Xemt on Github
+-- This is from what I remember (Can't access computer at the moment)
+players = game:GetService("Players")
+players.PlayerAdded:Connect(function(playa)
 
+
+end)
