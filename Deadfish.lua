64712592d4387dd4250cc4218de9bcbabaf8660d
Xemt <92655476+Xemt@users.noreply.github.com>
2022-07-28 07:49:11 -0400

    Update Deadfish.lua
    More changes, still kinda sleepy from staying up lol

--- Deadfish.lua
+++ Deadfish.lua
@@ -1,7 +1,21 @@
 -- Made by Xemt on Github
 -- This is from what I remember (Can't access computer at the moment)
+i = 0
+acc = 0
 players = game:GetService("Players")
 players.PlayerAdded:Connect(function(playa)
-
-
+ playa.Chatted:Connect(function(msg)
+   local lower = string.lower(msg)
+   local prefix = "/"
+   local args = string.split(lower, " ")
+   if args[0] == prefix.."deadfish" then
+    while i ~= #args[1] do
+     i = i + 1
+     achar = string.sub(args[1], i, i)
+     if achar == "i" then
+     
+     end
+    end
+   end
+  end)
 end)
