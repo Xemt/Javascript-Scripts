2df46c5cd85e3bb999bc867d68f4e55eedd6b407
Xemt <92655476+Xemt@users.noreply.github.com>
2022-07-28 09:17:14 -0400

    Update DeathScriptTest.lua

--- DeathScriptTest.lua
+++ DeathScriptTest.lua
@@ -3,6 +3,11 @@
 players = game:GetService("Players")
 players.PlayerAdded:Connect(function(playa)
  playa.Chatted:Connect(function(msg)
- 
+   local lower = string.lower(msg)
+   local prefix = "/"
+   local args = string.split(lower, " ")
+   if args[0] == prefix.."dthscrpt" then
+   
+   end
  end)
 end)
