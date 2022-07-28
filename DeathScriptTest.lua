bb394e246d5d46ef3c5191ad09fcb4404ef5b81e
Xemt <92655476+Xemt@users.noreply.github.com>
2022-07-28 09:13:45 -0400

    Update and rename DeathScript.lua to DeathScriptTest.lua
    Some changes

--- DeathScriptTest.lua
+++ DeathScriptTest.lua
@@ -1,4 +1,8 @@
 -- Made by Xemt on Github
 -- This is an attempt, and hasn't been tested yet
 players = game:GetService("Players")
-players.P
+players.PlayerAdded:Connect(function(playa)
+ playa.Chatted:Connect(function(msg)
+ 
+ end)
+end)
