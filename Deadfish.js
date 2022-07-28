ef2a4402f11d102988efbf3a4390ed2f984a5b99
Xemt <92655476+Xemt@users.noreply.github.com>
2022-07-28 07:21:44 -0400

    Create Deadfish.js
    Deadfish interpreter (I believe), made for the Scriptable app

---
+++ Deadfish.js
@@ -0,0 +1,36 @@
+// some code originally from "https://esolangs.org/wiki/Deadfish#HTML_.3F_Javascript"
+// Also this thing was made by Xemt on Github 😃😃
+var val = 0;
+var i = 0
+async function interpret() {
+ var input = new Alert()
+ input.title = "Input?"
+ input.addTextField()
+ input.addAction("OK")
+ await input.present()
+ var a = input.textFieldValue()
+ while (a.includes(" ")) {
+  a = a.replace(" ", "")
+ } // In case you accidentally put a space (or spaces) 😃
+ while (i != a.length) {
+  ach = a.charAt(i)
+  if(ach=='i'||ach=='I') {
+   val++
+  } 
+  if(ach=='d'||ach=='D') { 
+   val-- 
+  }
+  if(ach=='s'||ach == 'S') { 
+   val *= val
+  }
+  if(ach=='o'||ach == 'O') { 
+    var ascii = String.fromCharCode(val)
+    console.log(val + ", or " + ascii)
+  }
+  if (ach != 'i' && ach != 'I' && ach != 'd' && ach != 'D' && ach != 's' && ach != 'S' && ach != 'o' && ach != 'O') {
+   console.error("Unknown Command '" + ach + "'")
+  }
+  i++
+ }
+}
+interpret()
