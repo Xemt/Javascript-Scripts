f8dc1680cfb08ace93a5d1cc3646f94c49160063
Xemt <92655476+Xemt@users.noreply.github.com>
2022-11-30 17:00:42 -0600

    Update Deadfish.js

--- Deadfish.js
+++ Deadfish.js
@@ -4,13 +4,13 @@ if (input) {
  if (input.includes(" ")) {
   input = input.split("")
   for (let index = 0; index !== input.length; index++) {
-   if (input[index].match(/(\t|\r|\n|\h|\v\R\ )/g) {
+   if (input[index].match(/(\t|\r|\n|\h|\v\R\ )/g)) {
     input[index] = ""
    }
   }
   input = input.join("")
  }
  for (let index = 0; index !== input.length; index++) {
-  console.log(input[index])
+  console.log(input)
  }
 }
