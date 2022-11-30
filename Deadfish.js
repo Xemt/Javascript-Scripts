d7fad09826e7e7f429a99288dc22e0aa198c5ae7
Xemt <92655476+Xemt@users.noreply.github.com>
2022-11-30 16:59:22 -0600

    Create Deadfish.js

---
+++ Deadfish.js
@@ -0,0 +1,16 @@
+// Made By Xemt On Github
+var input = prompt("Deadfish code? ")
+if (input) {
+ if (input.includes(" ")) {
+  input = input.split("")
+  for (let index = 0; index !== input.length; index++) {
+   if (input[index].match(/(\t|\r|\n|\h|\v\R\ )/g) {
+    input[index] = ""
+   }
+  }
+  input = input.join("")
+ }
+ for (let index = 0; index !== input.length; index++) {
+  console.log(input[index])
+ }
+}
