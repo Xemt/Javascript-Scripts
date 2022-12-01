4b8fcea0d3f19b8679e2c505f5405e432bd95367
Xemt <92655476+Xemt@users.noreply.github.com>
2022-12-01 17:09:09 -0600

    Update Deadfish.js

--- Deadfish.js
+++ Deadfish.js
@@ -2,12 +2,9 @@
 var input = prompt("Deadfish code? ")
 if (input) {
  let accumulator = 0
- while (input.includes(" ")) {
-  input = input.replace(" ", "")
- }
+ while (input.includes(" ")) input = input.replace(" ", "")
  for (let index = 0; index !== input.length; index++) {
-  if (accumulator > 255 || accumulator < 0) {
-   accumulator = 0
-  }
+  if (accumulator > 255 || accumulator < 0) accumulator = 0
+  if (input[index] === "i") accumulator++; else if (input[index] === "d") accumulator--; else if (input[index] === "s") accumulator *= accumulator; else if (input[index]) === "o") alert(accumulator)
  }
 }
