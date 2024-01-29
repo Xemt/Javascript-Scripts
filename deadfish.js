e94877ffe97d2ea1905647e8e1082305d78cd18c
Xemt <92655476+Xemt@users.noreply.github.com>
2024-01-29 14:33:45 -0500

    Major changes, compatibility for Scriptable to ditch my old script.
    I made a version of this to work with the version of js that the Scriptable app uses.

---
+++ deadfish.js
@@ -0,0 +1,62 @@
+// Xemt, 7/28/22 - 1/29/24.
+
+"use strict";
+
+/**
+ * @description Quick and dirty version of prompt that works with Scriptable.
+ * @param {?string} msg
+ * @returns {any}
+*/
+async function my_prompt(msg = null)
+{
+	// We're using Scriptable, use Alert instead.
+	if ( typeof(_scriptable_run) == "function" ) {
+		let alert = new Alert();
+		let btn;
+
+		alert.title = "Prompt";
+		alert.message = msg;
+
+		alert.addAction("OK");
+		alert.addCancelAction("Cancel");
+
+		btn = alert.present().then(undefined, undefined);
+
+		if (btn == BTN_CANCEL) {
+			return "";
+		}
+
+		return alert.textFieldValue(0);
+	}
+
+	return prompt(msg);
+}
+
+var input = my_prompt("Deadfish code?").then(undefined, undefined);
+var acc = 0;
+var i = 0;
+
+while (i < input.length) {
+	if (acc > 255 || acc < -1) {
+		acc = 0;
+	}
+
+	switch (input.at[i]) {
+		case "i":
+			acc++;
+		break;
+		case "d":
+			acc--;
+		break;
+		case "s":
+			acc *= acc;
+		break;
+		case "o":
+			console.log(`accumulator: ${acc}`);
+		break;
+		default:
+			console.warn(`unknown deadfish command at ${i}`);
+		break;
+	}
+	i++;
+}
