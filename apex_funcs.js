e7339acf265cef28e52b58249d61fc18946c6673
Xemt <92655476+Xemt@users.noreply.github.com>
2024-04-15 17:31:46 -0400

    Update apex_funcs.js

--- apex_funcs.js
+++ apex_funcs.js
@@ -1,6 +1,6 @@
 /**
  * @author Xemt <https://github.com/Xemt/>.
- * @description 3/8/24 - 4/7/24. Useful functions for Apex Learning. Copy and
+ * @description 3/8/24 - 4/15/24. Useful functions for Apex Learning. Copy and
  * paste the code, and call whatever function you intend on using.
  *
  * MIT License
@@ -28,27 +28,29 @@
 
 "use strict";
 
-if ( !/apex(vs|learning)/.test(location.hostname) ) {
+if ( /apex(vs|learning)/.test(location.hostname) === false ) {
 	throw new Error("Must be on an Apex page!");
 }
-	
+
+var PAGENO_RE = /(?<=(\/page\/))\d+/;
+
 /**
- * @description Gets the question of whatever you're doing in Apex.
- * @throws {Error} 
- * @returns {String}
+ * @description Gets the question on the test or quiz that you're doing.
+ * @returns {?string} Returns null if we're not on a test or a quiz.
 */
-function apex_get_quest_txt()
+function apex_get_question()
 {
 	/* RegExp idea is from https:///www.youtube.com/shorts/scUvtg1B9I4. */
 	var html_re = /<(\/)?[^>]*>/;
-	var quest_elem = document.querySelector("kp-sia-question")
-													.children
-													.item(0)
-													.children
-													.item(0);
+
+	if (location.hostname.test() ) {
+		return null;
+	}
+	var quest_elem = document.querySelector("kp-sia-question").children.item(0).children.item(0);
 	var quest_txt = quest_elem.innerHTML;
 
-	while (html_re.test(quest_txt) === true) {
+	/* Strip any, and every HTML element. */
+	while ( html_re.test(quest_html) ) {
 		quest_txt = quest_txt.replace(html_re, "");
 	}
 
@@ -63,16 +65,11 @@ function apex_get_quest_txt()
 */
 function apex_goto_page(pageno)
 {
-	var pageno_re = /(?<=(\/page\/))\d+/;
-
-	if ((pageno < 0)  || 
-	    isNaN(pageno) ||
-	    (isFinite(pageno) == false))
-	{
+	if ((pageno < 0)  || isNaN(pageno) || (isFinite(pageno) == false)) {
 		throw new Error("apex_goto_page: Invalid argument.");
 	}
 
-	location.pathname = location.pathname.replace(pageno_re, pageno);
+	location.pathname = location.pathname.replace(PAGENO_RE, pageno);
 
 	return undefined;
 }
@@ -82,11 +79,16 @@ function apex_goto_page(pageno)
  * @returns {void}
 */
 function apex_goto_last_page()
-{
-	var pageno_re = /(?<=(\/page\/))\d+/;
+{	
+	if ( !location.pathname.test(PAGENO_RE) ) {
+		return undefined;
+	}
+	
 	/* The first number from the page number indicator. Example: "1" from
-     "1 of 5". */
+           "1 of 5". */
 	var last_pageno = document.querySelectorAll('[class="nav-section"]')[1].innerText.split(" ")[2];
 	
-	location.pathname = location.pathname.replace(pageno_re, last_pageno);
+	location.pathname = location.pathname.replace(PAGENO_RE, last_pageno);
+	
+	return undefined;
 }
